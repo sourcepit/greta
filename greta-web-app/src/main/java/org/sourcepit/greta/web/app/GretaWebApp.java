57b81e63bb0862fe2c34808ec83a046f34911dd5
Bernd Vogt <bernd.vogt@sourcepit.org>
2020-02-03 19:06:53 +0100

    feat: add spring boot web app

---
+++ greta-web-app/src/main/java/org/sourcepit/greta/web/app/GretaWebApp.java
@@ -0,0 +1,12 @@
+package org.sourcepit.greta.web.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GretaWebApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GretaWebApp.class, args);
+	}
+}
